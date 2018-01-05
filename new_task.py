cce81e92597296b25624102db5f835480370760b
spainior <ice_shi@163.com>
2018-01-05 16:36:21 +0800

    add queue message persitent

--- new_task.py
+++ new_task.py
@@ -4,13 +4,13 @@ import pika
 #create connection to localhost
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
 message = ''.join(sys.argv[1:]) or "Hello world"
 # message = 'Hello world'
 
 channel.basic_publish(exchange='',
-                      routing_key='hello',
+                      routing_key='task_queue',
                       body=message,
                       properties=pika.BasicProperties(
                       delivery_mode = 2, # make message persistent
--- worker.py
+++ worker.py
@@ -3,13 +3,17 @@ import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     time.sleep(body.count(b'.'))
     print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback, queue='task_queue')
 print(' [*] Waiting for message. To exit press Ctrl+ C')
+
+
 channel.start_consuming()
